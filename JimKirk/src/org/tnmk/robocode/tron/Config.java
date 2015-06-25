1c20b0ccb206e6973ce4365791ed62228ed81ac9
Khoi <khoi.tnm@gmail.com>
2015-06-26 01:09:38 +0700

    Add config

---
+++ JimKirk/src/org/tnmk/robocode/tron/Config.java
@@ -0,0 +1,11 @@
+package org.tnmk.robocode.tron;
+
+public class Config {
+	public static boolean FIRE = true;
+	public static boolean MOVE_CLOSE_TO_TARGET = true;
+	public static boolean CHANGE_DIRECTION_WHEN_FINISH_MOVE = true;
+	public static boolean CHANGE_DIRECTION_WHEN_BULLET_HIT = true;
+	public static boolean CHANGE_DIRECTION_WHEN_ROBOT_HIT = true;
+	public static boolean PANT_PREDICT_TARGET = false;
+	public static boolean MOVE_ONLY_IN_SAFE_ZONE = false;
+}
