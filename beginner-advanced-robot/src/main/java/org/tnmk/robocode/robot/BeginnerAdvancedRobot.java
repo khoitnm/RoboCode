73a49a33bc505968dbd2659f2c90b962a5b2e562
khoi.tnm <khoi.tnm@gmail.com>
2019-06-02 08:48:56 -0400

    Implement very basic of AdvancedRobot

--- beginner-advanced-robot/src/main/java/org/tnmk/robocode/robot/BeginnerAdvancedRobot.java
+++ beginner-advanced-robot/src/main/java/org/tnmk/robocode/robot/BeginnerAdvancedRobot.java
@@ -1,6 +1,7 @@
 package org.tnmk.robocode.robot;
 
 import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
 
 /**
  * The very simple robot which extends features from {@link AdvancedRobot}
@@ -9,14 +10,21 @@ import robocode.AdvancedRobot;
  */
 public class BeginnerAdvancedRobot extends AdvancedRobot {
     public void run() {
-        setAdjustRadarForRobotTurn(true);
-
-
         while (true) {
             ahead(100);
             turnGunRight(360);
+            //For basic Robot, you needs 2 ticks to execute the above 2 actions
+            //For advanced Robot, you only need 1 tick to execute them (by calling execute())
+            execute();
+
             back(100);
             turnGunRight(360);
+            execute();
         }
     }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+
 }
