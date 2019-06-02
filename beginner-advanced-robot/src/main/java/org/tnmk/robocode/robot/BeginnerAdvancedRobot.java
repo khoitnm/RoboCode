384140a646eb6fe9b89d324699332a7664a6c4bb
khoi.tnm <khoi.tnm@gmail.com>
2019-06-02 10:59:25 -0400

    Put some explanation for AdvanceRobot

--- beginner-advanced-robot/src/main/java/org/tnmk/robocode/robot/BeginnerAdvancedRobot.java
+++ beginner-advanced-robot/src/main/java/org/tnmk/robocode/robot/BeginnerAdvancedRobot.java
@@ -9,22 +9,41 @@ import robocode.ScannedRobotEvent;
  * http://mark.random-article.com/weber/java/robocode/lesson3.html
  */
 public class BeginnerAdvancedRobot extends AdvancedRobot {
+    private static int loopIndex = 0;
     public void run() {
         while (true) {
-            ahead(100);
+            ahead(80);
+            log("Advance Ahead 1");
             turnGunRight(360);
+            log("Advance Turn Gun Right 1");
+
             //For basic Robot, you needs 2 ticks to execute the above 2 actions
             //For advanced Robot, you only need 1 tick to execute them (by calling execute())
+            //  when you execute:
+            //  - the above action ahead() may need about 10 ticks (max velocity is 8) to finish,
+            //  - and turnGunRight() need 360/20=18 ticks to finish.
+            //  => So, in total, you need 18 ticks before the execute() is processed?
             execute();
+            log("Advance execute 1");
 
-            back(100);
+            back(80);
+            log("Advance Back");
             turnGunRight(360);
+            log("Advance Turn Gun Right 2");
             execute();
+            log("Advance execute 2");
+            loopIndex++;
         }
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
         fire(1);
+        log("Advance fire");
+    }
+
+    private void log(String message) {
+        String finalMessage = String.format("[%s] \t loop[%s] \t %s \t %s", this.getTime(), loopIndex, System.nanoTime(), message);
+        out.println(finalMessage);
     }
 
 }
--- beginner-basic-robot/src/main/java/org/tnmk/robocode/robot/BeginnerBasicRobot.java
+++ beginner-basic-robot/src/main/java/org/tnmk/robocode/robot/BeginnerBasicRobot.java
@@ -1,18 +1,32 @@
 package org.tnmk.robocode.robot;
 
-import robocode.*;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
 
 public class BeginnerBasicRobot extends Robot {
+    private static int loopIndex = 0;
+
     public void run() {
         while (true) {
-            ahead(100);
+            ahead(500);
+            log("Basic ahead 1");
             turnGunRight(360);
-            back(100);
+            log("Basic turn Gun Right 1");
+            back(500);
+            log("Basic back");
             turnGunRight(360);
+            log("Basic turn Gun Right 2");
+            loopIndex++;
         }
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
         fire(1);
+        log("Basic fight");
+    }
+
+    private void log(String message) {
+        String finalMessage = String.format("[%s] \t loop[%s] \t %s \t %s", this.getTime(), loopIndex, System.nanoTime(), message);
+        out.println(finalMessage);
     }
 }
