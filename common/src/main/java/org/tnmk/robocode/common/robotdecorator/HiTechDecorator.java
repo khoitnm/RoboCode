f0fd8cfb8862c9d17ff701ebe7b2b25726aa22b9
Khoi Tran <khoi.tnm@gmail.com>
2019-06-13 16:03:32 -0400

    Update Scan color.

--- common/src/main/java/org/tnmk/robocode/common/robotdecorator/HiTechDecorator.java
+++ common/src/main/java/org/tnmk/robocode/common/robotdecorator/HiTechDecorator.java
@@ -8,10 +8,13 @@ public class HiTechDecorator {
     private static final Color ROBOT_BORDY_COLOR = new Color(39, 33, 28);// new Color(51, 153, 153);
     private static final Color ROBOT_RADAR_COLOR = new Color(0, 166, 168);// Color(117, 209, 209);
     private static final Color ROBOT_GUN_COLOR = new Color(21, 202, 202);
+    private static final Color SCAN_COLOR = new Color(18, 163, 163);
+
 
     public static void decorate(Robot robot){
         robot.setBodyColor(ROBOT_BORDY_COLOR);
         robot.setRadarColor(ROBOT_RADAR_COLOR);
         robot.setGunColor(ROBOT_GUN_COLOR);
+        robot.setScanColor(SCAN_COLOR);
     }
 }
