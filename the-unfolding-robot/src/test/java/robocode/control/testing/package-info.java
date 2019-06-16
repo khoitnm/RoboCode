6402c71dd2305d2c20de5d757cc6ea79a32fc2dc
Khoi Tran <khoi.tnm@gmail.com>
2019-06-16 00:20:09 -0400

    Add UnitTest for robocode.

---
+++ the-unfolding-robot/src/test/java/robocode/control/testing/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package is copied from https://github.com/robo-code/robocode/tree/master/plugins/testing/robocode.testing.api/src/main/java/robocode/control/testing.
+ * That's the only way I can get the code because I don't see any Maven dependencies for that???
+ */
+package robocode.control.testing;
