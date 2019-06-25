4893ad0e86fd5da52c32e9be83b521bbd13e1a54
Kevin Tran <kevin.tran@leonardo.com>
2019-06-25 16:16:03 -0400

    Add UTurnRobot test

---
+++ beginner-advanced-robot/src/main/java/org/tnmk/robocode/robot/UTurnRobot.java
@@ -0,0 +1,65 @@
+package org.tnmk.robocode.robot;
+
+import org.tnmk.robocode.common.helper.BattleFieldUtils;
+import org.tnmk.robocode.common.log.LogHelper;
+import org.tnmk.robocode.common.movement.MovementContext;
+import org.tnmk.robocode.common.movement.tactic.uturn.UTurnMoveController;
+import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
+
+import java.awt.geom.Point2D;
+
+/**
+ * The robot for testing the {@link org.tnmk.robocode.common.movement.tactic.uturn.UTurnMoveController}
+ */
+public class UTurnRobot extends AdvancedRobot {
+    private static final double MOVE_DISTANCE = 200;
+
+    private final UTurnMoveController uTurnMoveController;
+    private final MovementContext movementContext;
+
+    public UTurnRobot() {
+        this.movementContext = new MovementContext(this);
+        this.uTurnMoveController = new UTurnMoveController(this, movementContext);
+    }
+
+    public void run() {
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+        setAdjustRadarForRobotTurn(true);
+
+
+        while (true) {
+            uTurnMoveController.runLoop();
+            if (uTurnMoveController.isStopped()) {
+                Point2D destination = calculateNewDestination(BattleFieldUtils.constructRobotPosition(this));
+                uTurnMoveController.setMoveToDestination(this, destination);
+            }
+            LogHelper.logRobotMovement(this, "Loop Movement");
+            execute();
+        }
+    }
+
+    private Point2D calculateNewDestination(Point2D currentPosition) {
+        Point2D centralBattle = new Point2D.Double(this.getBattleFieldWidth() / 2d, this.getBattleFieldHeight() / 2d);
+        double deltaX;
+        double deltaY;
+        if (currentPosition.getX() < centralBattle.getX()) {
+            deltaX = MOVE_DISTANCE;
+        } else {
+            deltaX = -MOVE_DISTANCE;
+        }
+        if (currentPosition.getY() < centralBattle.getY()) {
+            deltaY = MOVE_DISTANCE;
+        } else {
+            deltaY = -MOVE_DISTANCE;
+        }
+        return new Point2D.Double(currentPosition.getX() + deltaX, currentPosition.getY() + deltaY);
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        setFire(0.1);
+    }
+
+
+}
--- common/src/main/java/org/tnmk/robocode/common/movement/MoveController.java
+++ common/src/main/java/org/tnmk/robocode/common/movement/MoveController.java
@@ -1,4 +1,14 @@
 package org.tnmk.robocode.common.movement;
 
+import org.tnmk.robocode.common.robot.InitiableRun;
+import org.tnmk.robocode.common.robot.LoopableRun;
+import org.tnmk.robocode.common.robot.OnScannedRobotControl;
+
+/**
+ * This interface just represents a class which is used for control the movement.
+ * <p/>
+ * It can control by implementing {@link OnScannedRobotControl}, or {@link LoopableRun}, or {@link InitiableRun}, etc.<br/>
+ * It could act as a Movement strategy or a Movement tactic.
+ */
 public interface MoveController {
 }
