d29520cb8bb190c6f607fa504d0162746138d79c
Khoi Tran <khoi.tnm@gmail.com>
2019-06-25 20:14:00 -0400

    Refactor RandomMove: Change the condition before setting movement to none!

--- common/src/main/java/org/tnmk/robocode/common/movement/strategy/random/RandomMoveController.java
+++ common/src/main/java/org/tnmk/robocode/common/movement/strategy/random/RandomMoveController.java
@@ -73,6 +73,7 @@ public class RandomMoveController implements MoveController, LoopableRun, OnScan
      */
     private static final double EXCLUDE_CLOSET_POINT_PERCENTAGE = 0.2d;
     private static final double WANDER_DISTANCE = 150;
+    private static final long EXPIRED_PERIOD = 10;
 
     private final AdvancedRobot robot;
     private final AllEnemiesObservationContext allEnemiesObservationContext;
@@ -94,7 +95,7 @@ public class RandomMoveController implements MoveController, LoopableRun, OnScan
          * This case can happens when our robot cannot scan any enemy (but they are still there in the battlefield.
          * They are just outside the range of our radar.
          */
-        if (movementContext.isNone() && (robot.getTime() - estimateFinishTime) > 10) {
+        if (movementContext.isNone() && isExpiredMovement()) {
             movementContext.changeMoveStrategy(MoveStrategy.WANDERING, this);
             DebugHelper.debugMoveWandering(robot);
             startTime = robot.getTime();
@@ -106,12 +107,24 @@ public class RandomMoveController implements MoveController, LoopableRun, OnScan
             robot.setTurnRight(Math.random() * 360);
             robot.setAhead(direction * WANDER_DISTANCE);
         } else {
-            if ((movementContext.is(MoveStrategy.WANDERING) || movementContext.is(MoveStrategy.RANDOM)) && DoubleUtils.isConsideredZero(robot.getDistanceRemaining())) {
+            if (isWanderMoveFinished() || isRandomMoveFinished()) {
                 //When setting to none, it could be triggered by other algorithm. Otherwise, the wandering will be triggered again.
                 movementContext.setNone();
             }
             DebugHelper.resetDebugMoveWandering(robot);
         }
+    }
+
+    private boolean isWanderMoveFinished(){
+        return movementContext.is(MoveStrategy.WANDERING) && DoubleUtils.isConsideredZero(robot.getDistanceRemaining());
+    }
+
+    private boolean isRandomMoveFinished(){
+        return movementContext.is(MoveStrategy.RANDOM) && DoubleUtils.isConsideredZero(robot.getDistanceRemaining()) && isExpiredMovement();
+    }
+
+    private boolean isExpiredMovement(){
+        return (robot.getTime() - estimateFinishTime) > EXPIRED_PERIOD;
     }
 
     @Override
