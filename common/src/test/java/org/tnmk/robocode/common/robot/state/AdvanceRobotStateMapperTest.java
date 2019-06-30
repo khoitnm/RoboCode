605aa7112680414c751aa23586b010855db37e44
Khoi Tran <khoi.tnm@gmail.com>
2019-06-29 20:49:57 -0400

    Refactor AdvanceRobotFactory

--- common/src/test/java/org/tnmk/robocode/common/robot/state/AdvanceRobotStateMapperTest.java
+++ common/src/test/java/org/tnmk/robocode/common/robot/state/AdvanceRobotStateMapperTest.java
@@ -1,18 +1,15 @@
 package org.tnmk.robocode.common.robot.state;
 
-import java.util.Random;
 import org.junit.Assert;
 import org.junit.Test;
-import org.tnmk.robocode.common.constant.RobotPhysics;
 import robocode.AdvancedRobot;
-import robocode.Rules;
 
 public class AdvanceRobotStateMapperTest {
     private static final double DOUBLE_COMPARE_PRECISION = 0.0001;
 
     @Test
     public void testStateMapper() {
-        AdvancedRobot robot = constrcutAdvanceRobot();
+        AdvancedRobot robot = AdvanceRobotFactory.constructAdvanceRobot();
 
         AdvanceRobotState state = AdvanceRobotStateMapper.toState(robot);
 
@@ -39,31 +36,4 @@ public class AdvanceRobotStateMapperTest {
 
     }
 
-    private AdvancedRobot constrcutAdvanceRobot(){
-        AdvancedRobot robot = new AdvancedRobot();
-        MockAdvanceRobotPeer peer = new MockAdvanceRobotPeer();
-        robot.setPeer(peer);
-
-        peer.setName("Robot_" + System.nanoTime());
-        peer.setTime(new Random().nextLong());
-        peer.setEnergy(Math.random() * RobotPhysics.ROBOT_INITIATE_ENERGY);
-        peer.setX(Math.random() * 1200);
-        peer.setY(Math.random() * 1200);
-
-        peer.setVelocity(Math.random() * Rules.MAX_VELOCITY);
-        peer.setBodyHeading(Math.random() * 360);
-        peer.setGunHeading(Math.random() * 360);
-        peer.setRadarHeading(Math.random() * 360);
-
-        peer.setDistanceRemaining(Math.random() * 360);
-        peer.setBodyTurnRemaining(Math.random() * 360);
-        peer.setGunTurnRemaining(Math.random() * 360);
-        peer.setRadarTurnRemaining(Math.random() * 360);
-
-        peer.setGunHeat(Math.random() * 10);
-        peer.setOthers(new Random().nextInt(10));
-        peer.setRoundNum(new Random().nextInt(100));
-        peer.setNumSentries(new Random().nextInt(10));
-        return robot;
-    }
 }
