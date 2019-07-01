b8ab0e582010934e202d4f50bfb9afc1a14ef820
Khoi Tran <khoi.tnm@gmail.com>
2019-07-01 08:57:41 -0400

    Refactor RiskAreaAnalyst

--- common/src/main/java/org/tnmk/robocode/common/movement/strategy/antigravity/RiskAreaAnalyst.java
+++ common/src/main/java/org/tnmk/robocode/common/movement/strategy/antigravity/RiskAreaAnalyst.java
@@ -9,6 +9,10 @@ import org.tnmk.robocode.common.model.enemy.Enemy;
 
 public class RiskAreaAnalyst {
     public static final int NUM_OF_ANALYST_LEVELS = 2;
+    /**
+     * For each analyst level, the coefficient is 10.
+     */
+    public static final int RISK_LEVEL_COEFFICIENT = 10;
 
     public static List<RiskArea> findLeastRiskyArea(Rectangle2D battleField, Collection<Enemy> enemies) {
         List<RiskArea> areas = analyzeAreaRisks(battleField, NUM_OF_ANALYST_LEVELS, enemies);
@@ -41,7 +45,7 @@ public class RiskAreaAnalyst {
         }
 
         double areaRiskIgnoreLevel = analyseRiskIgnoreLevel(area, enemies);
-        double areaRiskByLevel = areaRiskIgnoreLevel * Math.pow(10, numAnalystLevels - analystLevel);
+        double areaRiskByLevel = areaRiskIgnoreLevel * Math.pow(RISK_LEVEL_COEFFICIENT, numAnalystLevels - analystLevel);
         double totalAreaRisk = parentAreaRisk + areaRiskByLevel;
         RiskArea riskArea = new RiskArea(area, totalAreaRisk);
 
@@ -59,6 +63,11 @@ public class RiskAreaAnalyst {
         }
     }
 
+    /**
+     * @param area
+     * @param enemies
+     * @return the result should less than {@link #RISK_LEVEL_COEFFICIENT}.
+     */
     private static double analyseRiskIgnoreLevel(Rectangle2D area, Collection<Enemy> enemies) {
         double risk = 0;
         for (Enemy enemy : enemies) {
