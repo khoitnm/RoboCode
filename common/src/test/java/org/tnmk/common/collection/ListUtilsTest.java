0f1f63539ed5be71be76d01e5320f79e1b932f5f
Kevin Tran <kevin.tran@leonardo.com>
2019-07-02 12:49:15 -0400

    Improve performance of ListUtils.findLeastValueItems

--- common/src/test/java/org/tnmk/common/collection/ListUtilsTest.java
+++ common/src/test/java/org/tnmk/common/collection/ListUtilsTest.java
@@ -9,19 +9,36 @@ import java.util.List;
 
 public class ListUtilsTest {
 
-    @Test
-    public void testGetLeastValue() {
-        List<SimplePerson> simplePersons = SimplePersonFactory.constructPersonsWithAges(5, 2, 4, 2, 6);
-        List<SimplePerson> leastAgeSimples = ListUtils.findLeastValueItems(simplePersons, SimplePerson::getAge);
-        Assert.assertEquals(2, leastAgeSimples.size());
-        Assert.assertTrue(leastAgeSimples.stream().allMatch(person -> person.getAge() == 2));
-    }
 
     @Test
-    public void testGetLeastValueWithEmptyList() {
+    public void testGetLeastValue_WithEmptyList() {
         List<SimplePerson> simplePersons = SimplePersonFactory.constructPersonsWithAges();
         List<SimplePerson> leastAgeSimples = ListUtils.findLeastValueItems(simplePersons, SimplePerson::getAge);
         Assert.assertTrue(leastAgeSimples.isEmpty());
         //No exception should be thrown when the list is empty!
+    }
+
+    @Test
+    public void testGetLeastValue_WithOneValue() {
+        List<SimplePerson> simplePersons = SimplePersonFactory.constructPersonsWithAges(3);
+        List<SimplePerson> leastAgeSimples = ListUtils.findLeastValueItems(simplePersons, SimplePerson::getAge);
+        Assert.assertEquals(1, leastAgeSimples.size());
+        Assert.assertTrue(leastAgeSimples.stream().allMatch(person -> person.getAge() == 3));
+    }
+
+    @Test
+    public void testGetLeastValue_WithSameValues() {
+        List<SimplePerson> simplePersons = SimplePersonFactory.constructPersonsWithAges(3, 3, 3, 3);
+        List<SimplePerson> leastAgeSimples = ListUtils.findLeastValueItems(simplePersons, SimplePerson::getAge);
+        Assert.assertEquals(4, leastAgeSimples.size());
+        Assert.assertTrue(leastAgeSimples.stream().allMatch(person -> person.getAge() == 3));
+    }
+
+    @Test
+    public void testGetLeastValue() {
+        List<SimplePerson> simplePersons = SimplePersonFactory.constructPersonsWithAges(7, 3, 5, 3, 4, 2, 2, 4, 2, 6, 2);
+        List<SimplePerson> leastAgeSimples = ListUtils.findLeastValueItems(simplePersons, SimplePerson::getAge);
+        Assert.assertEquals(4, leastAgeSimples.size());
+        Assert.assertTrue(leastAgeSimples.stream().allMatch(person -> person.getAge() == 2));
     }
 }
